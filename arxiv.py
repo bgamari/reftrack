a864251f37cbacb61fda30f049da4603cb777b9c
Ben Gamari <bgamari.foss@gmail.com>
2011-02-21 15:23:23 -0500

    Don't hack around XML prefixes

--- arxiv.py
+++ arxiv.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 import urllib2
-import xml.etree.ElementTree
+from xml.etree import ElementTree
 
 arxiv_oai_url = 'http://export.arxiv.org/oai2'
+
+prefixes = {'oai': 'http://www.openarchives.org/OAI/2.0/',
+            'arxiv': 'http://arxiv.org/OAI/arXiv/'}
 
 def oai_request(url, query):
         while True:
@@ -17,21 +20,22 @@ def oai_request(url, query):
                 else:
                         raise RuntimeError('Failed urllib2 request')
 
+def unpack_metadata(et):
+        md = {}
+        m = et.find('oai:GetRecord/oai:record/oai:metadata/arxiv:arXiv', namespaces=prefixes)
+        md['authors'] = [ (a.findtext('arxiv:forenames', namespaces=prefixes),
+                           a.findtext('arxiv:keyname', namespaces=prefixes))
+                         for a in m.findall('arxiv:authors/arxiv:author', namespaces=prefixes) ]
+        md['title'] = m.findtext('arxiv:title', namespaces=prefixes)
+        md['abstract'] = m.findtext('arxiv:abstract', namespaces=prefixes).strip()
+        return md
+
 def get_metadata(arxiv_id):
         query = 'verb=GetRecord&identifier=oai:arXiv.org:%s&metadataPrefix=arXiv' % arxiv_id
         d = oai_request(arxiv_oai_url, query)
-
-        # Hack around annoying root xmlns=... attributes
-        d = d.replace('xmlns="http://www.openarchives.org/OAI/2.0/"', '') 
-        d = d.replace('xmlns="http://arxiv.org/OAI/arXiv/"', '') 
-        et = xml.etree.ElementTree.fromstring(d)
-
-        md = { 'arxiv_id': arxiv_id }
-        m = et.find('GetRecord/record/metadata/arXiv')
-        md['authors'] = [ (a.findtext('forenames'), a.findtext('keyname'))
-                         for a in m.findall('authors/author') ]
-        md['title'] = m.findtext('title')
-        md['abstract'] = m.findtext('abstract').strip()
+        et = ElementTree.fromstring(d)
+        md = unpack_metadata(et)
+        md['arxiv_id'] = arxiv_id
         return md
 
 if __name__ == '__main__':
