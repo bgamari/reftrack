283916448e9909f67e7b03c4771fb14aa8582d6e
Ben Gamari <bgamari.foss@gmail.com>
2011-02-21 14:31:21 -0500

    metadata_extract: Beginnings of new algorithm

--- metadata_extract.py
+++ metadata_extract.py
@@ -1,13 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 import sys
 import re
 import subprocess
+from collections import namedtuple
 
 doi_re = re.compile(r'(?:doi|DOI)?[\s\.\:]{0,3}(10\.\d{4}/[\d\w\-\.\+]+)')
 arxiv_re = re.compile(r'arXiv:(\d{4,4}\.\d{4,4})(v\d+)?')
 
-def find_metadata(file):
+def find_ids(file):
         metadata = {}
         p = subprocess.Popen(['pdftotext', file, '-'], stdout=subprocess.PIPE)
         txt = p.stdout.read()
@@ -21,45 +22,48 @@ def find_metadata(file):
                 metadata['arxiv_id'] = m.group(1)
 
         return metadata
-        
-def find_doi_pdfminer(file):
-        from pdfminer.converter import PDFPageAggregator
-        from pdfminer.layout import LAParams, LTTextBox, LTTextLine
-        from pdfminer.pdfparser import PDFDocument, PDFParser
-        from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 
-        rsrc = PDFResourceManager()
-        device = PDFPageAggregator(rsrc, laparams=LAParams())
-        doc = PDFDocument()
-        fp = open(file)
-        parser = PDFParser(fp)
-        parser.set_document(doc)
-        doc.set_parser(parser)
-        doc.initialize('')
-        interpreter = PDFPageInterpreter(rsrc, device)
+def flatten_text(el):
+        """ Returns the concatenated text of all children of element el """
+        return ''.join(el.itertext())
 
-        pages = list(doc.get_pages())
-        interpreter.process_page(pages[0])
-        layout = device.get_result()
+def find_metadata(file):
+        from xml.etree import ElementTree
+        metadata = {}
+        p = subprocess.Popen(['pdftohtml', '-xml', '-stdout', file], stdout=subprocess.PIPE)
+        s = p.stdout.read()
+        et = ElementTree.fromstring(s)
+        first_page = et.find('page')
+        page_h, page_w = first_page.attrib['height'], first_page.attrib['width']
 
-        print sys.argv[1], layout.__dict__
-        for n,lt_obj in enumerate(layout._objs):
-                if isinstance(lt_obj, LTTextBox):
-                        m = doi_re.search(lt_obj.text)
-                        if m:
-                                print m.group(1),
-                        height = lt_obj._objs[0].height
-                        print 'TextBox (%f): %s' % (height, lt_obj.text.encode('iso-8859-1', 'ignore'))
-                
-                if isinstance(lt_obj, LTTextLine):
-                        print 'TextLine: %s (%f)' % (lt_obj.text.encode('iso-8859-1'), lt_obj.height)
+        # Map font ids to sizes
+        font_ids = { fs.attrib['id']: float(fs.attrib['size']) for fs in first_page.findall('fontspec') }
+        found_fonts = [ el.attrib['font'] for el in first_page.findall('text') ]
+        # List of font ids found on first page sorted by size
+        sorted_font_ids = sorted(found_fonts, key=font_ids.get, reverse=True)
 
-                if n == 5: break
+        # To find the title we look for largest text on first page.  When we
+        # find multiple lines we check to see if they are a single line by
+        # comparing the difference in their y positions to their height
+        largest_text = first_page.findall('text[@font="%s"]' % sorted_font_ids[0])
+        title = ''
+        last_y = float(largest_text[0].attrib['top'])
+        for t in largest_text:
+                y = float(t.attrib['top'])
+                height = float(t.attrib['height'])
+                if abs(last_y - y) / height > 1.5:
+                        break
+                title += ' ' + flatten_text(t)
+                last_y = y
 
-        print
+        # Find authors: Look for second largest text on page
+        text = first_page.findall('text[@font="%s"]' % sorted_font_ids[1])
+        print map(flatten_text, text)
+
+        return title
 
 if __name__ == '__main__':
         import sys
         for f in sys.argv[1:]:
-                print f, find_doi(f)
+                print f, find_metadata(f)
 
