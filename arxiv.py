607facaa3479d57afd74dd7e1c18467a5abbd39a
Ben Gamari <bgamari.foss@gmail.com>
2011-02-21 14:29:38 -0500

    Factor out OAI query submission

--- arxiv.py
+++ arxiv.py
@@ -5,9 +5,22 @@ import xml.etree.ElementTree
 
 arxiv_oai_url = 'http://export.arxiv.org/oai2'
 
+def oai_request(url, query):
+        while True:
+                r = urllib2.urlopen('%s?%s' % (url, query))
+                if r.code == 503:
+                        from time import sleep
+                        wait = float(r.info()['Retry-After'])
+                        sleep(wait)
+                if r.code == 200:
+                        return r.read()
+                else:
+                        raise RuntimeError('Failed urllib2 request')
+
 def get_metadata(arxiv_id):
-        r = urllib2.urlopen('%s?verb=GetRecord&identifier=oai:arXiv.org:%s&metadataPrefix=arXiv' % (arxiv_oai_url, arxiv_id))
-        d = r.read()
+        query = 'verb=GetRecord&identifier=oai:arXiv.org:%s&metadataPrefix=arXiv' % arxiv_id
+        d = oai_request(arxiv_oai_url, query)
+
         # Hack around annoying root xmlns=... attributes
         d = d.replace('xmlns="http://www.openarchives.org/OAI/2.0/"', '') 
         d = d.replace('xmlns="http://arxiv.org/OAI/arXiv/"', '') 
@@ -23,6 +36,6 @@ def get_metadata(arxiv_id):
 
 if __name__ == '__main__':
         arxiv_id = '0804.2273'
-        metad = get_arxiv_metadata(arxiv_id)
+        metad = get_metadata(arxiv_id)
         print metad
 
