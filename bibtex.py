5d2caa944c17df8d8622abc68c321540ba16bc40
Ben Gamari <bgamari.foss@gmail.com>
2011-02-22 10:59:29 -0500

    Change hash-bangs to python2.7

--- bibtex.py
+++ bibtex.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 def format_bibtex(ref, refname):
         types = {
--- crossref.py
+++ crossref.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 
 import urllib
 import urllib2
