fced11582532ae2ceb3b9472425dde4db786f54d
Ben Gamari <bgamari.foss@gmail.com>
2011-07-13 19:22:27 -0400

    database: Add date formatting routines

--- database.py
+++ database.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
 
 import couchdb
+from datetime import datetime
 
 search_server = 'http://localhost:5984/'
-refs = couchdb.Server()['reftrack_refs']
-docs = couchdb.Server()['reftrack_documents']
+server = couchdb.Server()
+for db in ['reftrack_refs', 'reftrack_documents']:
+        if db not in server: server.create(db)
+
+refs = server['reftrack_refs']
+docs = server['reftrack_documents']
+
+def parse_date(str):
+        return datetime.strptime(str, '%Y-%m-%d')
+
+def format_date(date):
+        return datetime.strftime(date, '%Y-%m-%d')
 
