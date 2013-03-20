83a27be8a04816612aef73d59d9e92fc2ca14344
Ben Gamari <bgamari.foss@gmail.com>
2013-03-20 19:03:15 -0400

    PDFScrape: Whitespace cleanup

--- PDFScrape.hs
+++ PDFScrape.hs
@@ -5,8 +5,8 @@ import System.Environment
 import Control.Error
 import Data.Acid
 import Data.Acid.Remote
-import Network       
-       
+import Network
+
 import RefTrack.Types
 import RefTrack.ScrapeIds
 import RefTrack.CrossRef
@@ -15,14 +15,14 @@ findPdfIds :: FilePath -> IO [ExternalRef]
 findPdfIds file = do
     (code, contents, err) <- readProcessWithExitCode "pdftotext" [file, "-"] ""
     return $ findIds contents
- 
+
 lookupId :: ExternalRef -> IO (Maybe Ref)
 lookupId (DOIRef doi) = do
     res <- runEitherT $ lookupDoi doi
     case res of
         Left err -> errLn err >> return Nothing
         Right a  -> return a
-    
+
 lookupId _ = return Nothing
 
 scrape :: FilePath -> IO (Maybe ExternalRef, Maybe Ref)
@@ -35,17 +35,16 @@ scrape f = do
            , case catMaybes refs of a:[] -> Just a
                                     _    -> Nothing
            )
-    
+
 main = do
     args <- getArgs
     (eids, refs) <- unzip `fmap` forM args scrape
     print $ "Found IDs for "++frac (length $ catMaybes eids) (length eids)
     print $ "Found refs for "++frac (length $ catMaybes refs) (length eids)
-    
+
     st <- openRemoteState "localhost" (PortNumber 7777)
     mapM_ (update st . AddRef) $ catMaybes refs
     closeAcidState st
-    
+
 frac :: Real n => n -> n -> String
 frac a b = show (100 * realToFrac a / realToFrac b)++"%"
-
