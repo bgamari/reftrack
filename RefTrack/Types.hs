4b89d753cf9414e9b38c1f5212bc76dfda554683
Ben Gamari <bgamari.foss@gmail.com>
2013-03-20 18:50:44 -0400

    Types: Enable -XPatternGuards

--- RefTrack/Types.hs
+++ RefTrack/Types.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving,
              DeriveDataTypeable, TemplateHaskell, TypeFamilies,
-             OverloadedStrings #-}
+             OverloadedStrings, PatternGuards #-}
 
 module RefTrack.Types where
 
