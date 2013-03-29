40745d9e5b01f9124b326225a7dd5a6a48ca85b3
Ben Gamari <bgamari.foss@gmail.com>
2013-03-29 17:45:52 -0400

    TestCrossRef: Fix lookupDoi usage

--- TestCrossRef.hs
+++ TestCrossRef.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Config
 import RefTrack.Types
 import RefTrack.CrossRef
 import Control.Error
 import Control.Monad
 
 main = do
-  forM_ dois $ \doi->print =<< runEitherT (lookupDoi doi)
-  
+  forM_ dois $ \doi->print =<< runEitherT (lookupDoi crossrefServer doi)
+
 dois = map DOI ["10.1577/H02-043", "10.1021/jp035514+"]
