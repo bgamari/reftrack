53f5239b5df8e5430f935051514fafd8777a87e9
Ben Gamari <bgamari.foss@gmail.com>
2013-03-30 11:31:13 -0400

    PDFScrape: Add documents

--- PDFScrape.hs
+++ PDFScrape.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+
 import System.Process
 import Data.Maybe
 import Control.Monad
@@ -6,11 +8,16 @@ import Control.Error
 import Data.Acid
 import Data.Acid.Remote
 import Network
+import qualified Data.ByteString.Lazy as LBS
+import Data.Time.Clock
+import Control.Applicative
 
 import RefTrack.Types
 import RefTrack.ScrapeIds
 import RefTrack.CrossRef
 import Config
+
+import Data.Digest.Pure.SHA
 
 findPdfIds :: FilePath -> IO [ExternalRef]
 findPdfIds file = do
@@ -26,25 +33,36 @@ lookupCrossref s (DOIRef doi) = do
 
 lookupCrossref _ _ = return Nothing
 
-scrape :: FilePath -> IO (Maybe ExternalRef, Maybe Ref)
+scrape :: FilePath -> IO (Maybe Ref)
 scrape f = do
     ids <- findPdfIds f
     print (f,ids)
     refs <- mapM (lookupCrossref crossrefServer) ids
     print refs
-    return ( listToMaybe ids
-           , case catMaybes refs of a:[] -> Just a
+    return $ case catMaybes refs of a:[] -> Just a
                                     _    -> Nothing
-           )
+
+buildDocument :: RefId -> FilePath -> IO Document
+buildDocument refId fname = do
+    d <- LBS.readFile fname
+    today <- getCurrentTime
+    return $ Document { _docHash     = SHAHash $ LBS.toStrict $ bytestringDigest $ sha256 d
+                      , _docTitle    = Nothing
+                      , _docFilename = fname
+                      , _docRef      = refId
+                      , _docDateImported = today
+                      }
 
 main = do
     args <- getArgs
-    (eids, refs) <- unzip `fmap` forM args scrape
-    putStrLn $ "Found IDs for "++frac (length $ catMaybes eids) (length eids)
-    putStrLn $ "Found refs for "++frac (length $ catMaybes refs) (length eids)
+    refs <- forM args $ \f->fmap (f,) <$> scrape f
+    putStrLn $ "Found refs for "++frac (length $ catMaybes refs) (length refs)
 
     st <- openRemoteState "localhost" (PortNumber 7777)
-    mapM_ (update st . AddRef) $ catMaybes refs
+    forM_ (catMaybes refs) $ \(fname,ref) -> do
+        refId <- update st $ AddRef ref
+        doc <- buildDocument refId fname
+        update st $ AddDocument doc
     closeAcidState st
 
 frac :: Real n => n -> n -> String
